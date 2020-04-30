38964d162621bcbf97cfabb494bc518c2512ceae
caoqixin <caoqixin7@gmail.com>
2020-04-30 17:50:10 +0200

    '邮箱验证'

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 use Illuminate\Support\Facades\Route;
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
