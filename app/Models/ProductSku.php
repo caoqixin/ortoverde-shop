f03ab95c282ba1abf96da3a80be870111cd2f241
caoqixin <caoqixin7@gmail.com>
2020-05-03 15:40:51 +0200

    '商品模型'

---
+++ app/Models/ProductSku.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductSku extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'stock',
+    ];
+
+    // 关联products
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
