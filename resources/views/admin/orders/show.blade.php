3b8aca260d7ae9485c76a9f9ef2d7b4cc53a2a0a
caoqixin <caoqixin7@gmail.com>
2020-05-13 18:23:07 +0200

    '管理后台-订单发货'

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -42,8 +42,50 @@
             @endforeach
             <tr>
                 <td>订单金额:</td>
-                <td colspan="3"> &euro; {{ $order->total_amount }}</td>
+                <td> &euro; {{ $order->total_amount }}</td>
+                <td>发货状态:</td>
+                <td class="bg-success">{{ \App\Models\Order::$shipStatusMap[$order->ship_status] }}</td>
             </tr>
+            {{--            订单开始发货--}}
+            {{--            如果订单未发货, 显示发货表单--}}
+            @if($order->ship_status === \App\Models\Order::SHIP_STATUS_PENDING)
+                <tr>
+                    <td colspan="4">
+                        <form action="{{ route('admin.orders.ship', ['order' => $order->id]) }}" method="post" class="form-inline">
+                            {{ csrf_field() }}
+                            <div class="form-group {{ $errors->has('contact_name') ? 'has-error' : ''}} ">
+                                <label for="contact_name" class="control-label">送货人姓名</label>
+                                <input type="text" id="contact_name" name="contact_name" value="" class="form-control"
+                                       placeholder="送货人姓名">
+                                @if($errors->has('contact_name'))
+                                    @foreach($errors->get('contact_name') as $msg)
+                                        <span class="help-block">{{ $msg }}</span>
+                                    @endforeach
+                                @endif
+                            </div>
+                            <div class="form-group {{ $errors->has('contact_phone') ?'has-error':'' }}">
+                                <label for="contact_phone" class="control-label">送货人联系电话</label>
+                                <input type="text" id="contact_phone" name="contact_phone" value="" class="form-control"
+                                       placeholder="输入送货人联系方式">
+                                @if($errors->has('contact_phone'))
+                                    @foreach($errors->get('contact_phone') as $msg)
+                                        <span class="help-block">{{ $msg }}</span>
+                                    @endforeach
+                                @endif
+                            </div>
+                            <button type="submit" class="btn btn-success" id="ship-btn">发货</button>
+                        </form>
+                    </td>
+                </tr>
+            @else
+                {{--                否则展示送货人联系方式--}}
+                <tr>
+                    <td>送货人姓名:</td>
+                    <td class="bg-primary">{{ $order->ship_data['contact_name'] }}</td>
+                    <td>送货人联系方式:</td>
+                    <td class="bg-info">{{ $order->ship_data['contact_phone'] }}</td>
+                </tr>
+            @endif
             </tbody>
         </table>
     </div>
