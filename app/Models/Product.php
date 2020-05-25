410b42ac209bd9e9b1f26592c1db4dcf546ccd0d
caoqixin <caoqixin7@gmail.com>
2020-05-25 18:38:11 +0200

    初始化类目模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -28,6 +28,11 @@ class Product extends Model
         return $this->hasMany(ProductSku::class);
     }
 
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
     // 转化图片地址
     public function getImageUrlAttribute()
     {
