b75417091972523aca711a6b5b2daa3c5e0b17f7
caoqixin <caoqixin7@gmail.com>
2020-05-27 19:22:34 +0200

    前台商品列表支持按类目筛选

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
+use App\Models\Category;
 use App\Models\OrderItem;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -30,6 +31,21 @@ class ProductsController extends Controller
             });
         }
 
+        // 如果有传入category_id 字段, 并且在数据库中有对应的类目
+        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
+            // 如果是一个父类目
+            if ($category->is_directory) {
+                // 筛选该父类目所有的子类类目商品
+                $builder->whereHas('category', function ($query) use ($category) {
+                    $query->where('path', 'like', $category->path . $category->id . '-%');
+                });
+            } else {
+                // 如果不是一个父类目, 直接筛选此类目下的商品
+                $builder->where('category_id', $category->id);
+            }
+
+        }
+
         // 是否有提交 order 参数
         // order 参数用来控制商品排序
         if ($order = $request->input('order', '')) {
@@ -51,7 +67,8 @@ class ProductsController extends Controller
             'filters' => [
                 'search' => $search,
                 'order' => $order,
-            ]
+            ],
+            'category' => $category ?? null,
         ]);
     }
 
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -11,6 +11,27 @@
                         <div class="form-row">
                             <div class="col-md-9">
                                 <div class="form-row">
+                                    {{--                                    面包屑开始--}}
+                                    <div class="col-auto category-breadcrumb">
+                                        {{--                                        添加一个名为全部的连接, 直接跳转到商品列表--}}
+                                        <a href="{{ route('products.index') }}" class="all-products">全部</a>
+                                        {{--                                        如果当前是通过类目筛选的--}}
+                                        @if($category)
+                                            {{--                                            遍历这个类目的所有祖先类目，我们在模型的访问器中已经排好序，因此可以直接使用--}}
+                                            @foreach($category->ancestors as $ancestor)
+                                                {{--                                                添加一个名为该祖先类目的连接--}}
+                                                <span class="category">
+                                                    <a href="{{ route('products.index', ['category_id' => $ancestor->id]) }}">{{ $ancestor->name }}</a>
+                                                </span>
+                                                <span>&gt;</span>
+                                            @endforeach
+                                            {{--                                            展示当前类目名称--}}
+                                            <span class="category">{{ $category->name }}</span><span> ></span>
+                                            {{--                                           当前类目的 ID，当用户调整排序方式时，可以保证 category_id 参数不丢失 --}}
+                                            <input type="hidden" name="category_id" value="{{ $category->id }}">
+                                        @endif
+                                    </div>
+                                    {{--                                    面包屑结束--}}
                                     <div class="col-auto"><input type="text" class="form-control form-control-sm"
                                                                  name="search" placeholder="搜索"></div>
                                     <div class="col-auto">
@@ -31,7 +52,23 @@
                             </div>
                         </div>
                     </form>
-                    <!-- 筛选组件结束 -->
+                    {{--                    展示子类目--}}
+                    <div class="filters">
+                        {{--                        如果当前是通过类目筛选, 并且此类目是父类--}}
+                        @if($category && $category->is_directory)
+                            <div class="row">
+                                <div class="col-3 filter-key">子类目:</div>
+                                <div class="col-9 filter-values">
+                                    @foreach($category->children as $child)
+                                        <a href="{{ route('products.index', ['category_id' => $child->id]) }}">{{ $child->name }}</a>
+                                    @endforeach
+                                </div>
+                            </div>
+
+                        @endif
+                    </div>
+                {{--                    结束展示子类目--}}
+                <!-- 筛选组件结束 -->
                     <div class="row products-list">
                         @foreach($products as $product)
                             <div class="col-3 product-item">
